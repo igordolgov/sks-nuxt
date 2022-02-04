1d75d84a62ff695e467c54c7be9ed69fa895b358
Igor Dolgov <65899955+igordolgov@users.noreply.github.com>
2022-02-04 08:31:01 +0300

    first upload

---
+++ layouts/default.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-app>
+    <v-main class="grey lighten-2">
+      <v-container>
+        <MainCatalog />
+        <!-- Аналог vue-router, указывает место, где рендерятся компоненты -->
+        <!-- <Nuxt /> -->
+      </v-container>
+    </v-main>
+
+    <!-- <v-footer :absolute="!fixed" app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer> -->
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/',
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire',
+        },
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js',
+    }
+  },
+}
+</script>
+
+<style scoped>
+  /* main {
+    background-color: red;
+  } */
+</style>
---
+++ pages/products/_id.vue
@@ -0,0 +1,5 @@
+<template>
+    <section>
+        <h1 class="yellow red--text">Товар №{{ $route.params.id }}</h1>
+    </section>
+</template>
